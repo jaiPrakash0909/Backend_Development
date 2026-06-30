d509ca531e7e8adc4902d591a54b00a2db613608
Jai prakash Banjare <jaiprakashbanjare932@gmail.com>
2026-06-30 23:35:51 +0530

    Enhance middleware with request logging, authentication, role-based access, and rate limiting

--- Express/03_middleware.js
+++ Express/03_middleware.js
@@ -6,8 +6,15 @@ function Block_1_httpMethods(){
 
         const logs = []
 
-        app.use(express.json())
+        app.use(express.json({limit: '50kb'}))
+        app.use(express.urlencoded({extended: true, limit: '50'}))
+        app.use(express.static(root, {
+            dotfiles: 'ignore',
+            maxAge: 0
+        }))
 
+
+        
         // request logger
         // next is use when you want to write middleware
         app.use((req, res, next) => {
@@ -15,8 +22,6 @@ function Block_1_httpMethods(){
             // console log everything
             // write in some file
             // authenticate a user
-
-
 
             const logEntry = `${req.method} : ${req.url}`
             logs.push(logEntry)
@@ -31,6 +36,7 @@ function Block_1_httpMethods(){
 
             res.on('finish', () => {
                 const duration = Date.now() - req.startTime;
+                console.log(`[TIMER] - ${req.method} - ${req.url} took ${duration}ms`)
             })
 
 
@@ -39,7 +45,59 @@ function Block_1_httpMethods(){
             next()
         })
 
+        function authMe (req, res, next) {
+            const token = req.headers['x-auth-token']
 
+            if(!token){
+                return res.status(401).json({error:"No Token, please login"})
+            }
+
+            if(token !== "secret-code"){
+                return res.status(403).json({error: "Invalid token"})
+            }
+
+            // token -> extract data from token -> userID, email, admin
+
+            req.user = {id: 1, name: "jai", role: "admin"}
+
+            next()
+        }
+
+        function getRole(role) {
+            return (req, res, next) => {
+                if(!req.user || req.user.role !== role){
+                    return res.status(403).json({error: `Role ${role} required`})
+                }
+                next() 
+            }
+        }
+
+// Rate limit
+        function rateLimit(maxRequest){
+            let count = 0
+
+            return (req, res, next) => {
+                count++
+                if(count > maxRequest){
+                    return res.status(429).json({error: "Too many request, please try after some time"})
+                }
+                next()
+            }
+        }
+
+        const limitedEndPoint = rateLimit(3)
+
+        app.get('/limited', limitedEndPoint,(req, res) => {
+            
+        })
+
+
+
+        app.get('/profile', authMe, getRole('admin') ,() => {})
+        app.get('/profile', authMe, getRole('teacher') ,() => {})
+        app.get('/profile', authMe, getRole('student') ,() => {})
+        app.get('/profile', authMe, getRole('admin') ,() => {})
+        app.get('/profile', authMe, getRole(['admin', 'teacher', 'student']) ,() => {})
 
 
 
@@ -51,8 +109,6 @@ function Block_1_httpMethods(){
             
             try{
             
-
-
             }
             catch (error){
                 console.log(error)
@@ -65,6 +121,9 @@ function Block_1_httpMethods(){
         })
     })
 }
+
+
+
 
 
 async function main(){
