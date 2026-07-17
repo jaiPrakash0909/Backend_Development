aec9eb720a91e1d37edb0664ed2f57bba2b8e575
Jai prakash Banjare <jaiprakashbanjare932@gmail.com>
2026-07-17 19:40:03 +0530

    Add DTOs for forgot and reset password functionality

--- RestFull_API/src/modules/auth/dto/register.dto.js
+++ RestFull_API/src/modules/auth/dto/register.dto.js
@@ -1,16 +1,19 @@
 import joi from "joi";
-import BaseDto from "../../../common/dto/base.dto.js"
-
+import BaseDto from "../../../common/dto/base.dto.js";
 
 class RegisterDto extends BaseDto {
     static schema = joi.object({
-        name: joi.string().trim().min(2).max(50).required(),
-        email: joi.string().email().lowercase().required(),
-        password: joi.string()
-        .message("Password must contain 8 chars minimum")
-        .min(8).required(),
-        role: joi.string().valid("customer", "seller").default("customer")
-    })
+    name: Joi.string().trim().min(2).max(50).required(),
+    email: Joi.string().email().lowercase().required(),
+    password: Joi.string()
+        .min(8)
+        .pattern(/(?=.*[A-Z])(?=.*\d)/)
+        .message(
+        "Password must contain at least one uppercase letter and one digit",
+        )
+        .required(),
+    role: Joi.string().valid("customer", "seller").default("customer"),
+    });
 }
 
-export default RegisterDto
+export default RegisterDto;
