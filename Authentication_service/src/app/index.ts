003c2e1bae43aabbd13954c59605e36ce84ddd8b
Jai prakash Banjare <jaiprakashbanjare932@gmail.com>
2026-07-18 21:30:12 +0530

    signUp route

--- Authentication_service/src/app/index.ts
+++ Authentication_service/src/app/index.ts
@@ -1,16 +1,21 @@
 import express from "express";
 import type { Express } from 'express'
 
+import { authRouter } from "./auth/routes";
+
 export function createApplication(): Express {
     const app = express()
 
     //Middleware
+    app.use(express.json())
 
 
     // Routes
     app.get('/', (req, res) => {
         return res.json({message: 'Welcome to auth Service'})
     })
+
+    app.use('/auth', authRouter)
 
     return app
 }
