544b9c0635cf3aa45b1324733df5b70fde265514
Jai prakash Banjare <jaiprakashbanjare932@gmail.com>
2026-07-21 23:48:47 +0530

    Implement async file operations to read from a.txt and write to b.txt

--- Reading_and_Writing_files/src/modules/fs-module/fs-async.js
+++ Reading_and_Writing_files/src/modules/fs-module/fs-async.js
@@ -1,3 +1,4 @@
+import { error } from "node:console"
 import fs from "node:fs"
 
 
@@ -14,3 +15,13 @@ import fs from "node:fs"
 //     }
 //     console.log("READ:", data)
 // })
+
+fs.readFile("a.txt", "utf-8", (error, data)=>{
+    fs.writeFile("b.txt", data, (err) => {
+        fs.appendFile("b.txt", "\nDone", (err)=> {
+            fs.unlink("a.txt", (err)=> {
+                console.log("a.txt deleted")
+            })
+        })
+    })
+})
