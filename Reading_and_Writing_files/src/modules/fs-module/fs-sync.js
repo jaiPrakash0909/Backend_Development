d7e37a59e8c13fc25894bb26c4a0b7e5dce10107
Jai prakash Banjare <jaiprakashbanjare932@gmail.com>
2026-07-21 23:25:31 +0530

    fs-sync module files with console log and comments

---
+++ Reading_and_Writing_files/src/modules/fs-module/fs-sync.js
@@ -0,0 +1,25 @@
+import fs from "node:fs"
+
+//1. WRITE 
+
+// fs.writeFileSync("test.txt", "Hello from sync fs!",)
+
+// 2. READ
+// const data = fs.readFileSync("test.txt")
+
+// console.log(data)
+
+// fs.appendFileSync("test.txt", "\nHello from apend")
+
+// fs.mkdirSync("myFolder/innerFolder", {recursive: true})
+
+// delete file
+// fs.unlinkSync("test.txt")
+
+// fs.renameSync("test.txt", "test1.js")
+
+// fs.cpSync("test1.txt", "finalText.txt")
+
+//delete empty folder
+// fs.rmdirSync("myFolder")
+
