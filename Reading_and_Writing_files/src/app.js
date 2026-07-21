fef193955867dea240af47ffa748e59ed66bc63d
Jai prakash Banjare <jaiprakashbanjare932@gmail.com>
2026-07-22 03:09:38 +0530

    Add file upload endpoint using multer in app.js

--- Reading_and_Writing_files/src/app.js
+++ Reading_and_Writing_files/src/app.js
@@ -3,11 +3,28 @@ import express from "express";
 import authRoute from "./modules/auth/auth.routes.js";
 import ApiError from "./common/utils/api-error.js";
 import multer from "multer";
+import ApiResponse from "./common/utils/api-response.js";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+
+
+
+const upload = multer();
+
+app.post("/upload", upload.single("file"), (req, res)=>{
+  console.log(req.file)
+
+  ApiResponse.ok(req, "File uploaded")
+})
+
+
+
+
+
 
 app.use("/api/auth", authRoute);
 
@@ -15,4 +32,7 @@ app.use("/api/auth", authRoute);
 app.all("{*path}", (req, res) => {
   throw ApiError.notFound(`Route ${req.originalUrl} not found`);
 });
+
+
+
 export default app;
