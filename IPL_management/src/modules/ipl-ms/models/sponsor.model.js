a7765faba90988fbee77f01f461d58f61a9e2668
Jai prakash Banjare <jaiprakashbanjare932@gmail.com>
2026-07-31 14:32:45 +0530

    Add sponsor model schema for IPL management

---
+++ IPL_management/src/modules/ipl-ms/models/sponsor.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const sponsorSchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required: [true, "Sponsor name is required"],
+            trim: true,
+            minlength: 2,
+            maxlength: 100,
+        },
+        
+    },
+    {timestamps: true},
+)
+
+export default mongoose.model("Sponsor", sponsorSchema)
