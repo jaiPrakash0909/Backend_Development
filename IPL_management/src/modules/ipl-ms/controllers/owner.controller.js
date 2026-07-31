d6680dcea531a2744fed980fc0e3dff7c54b88ec
Jai prakash Banjare <jaiprakashbanjare932@gmail.com>
2026-07-31 17:22:13 +0530

    Add owner controller and service for IPL management

---
+++ IPL_management/src/modules/ipl-ms/controllers/owner.controller.js
@@ -0,0 +1,18 @@
+
+const creatOwner = async(req, res) => {};
+
+const getAllOwners = async(req, res) => {};
+
+const getOwnerById = async(req, res) => {};
+
+const updateOwner = async(req, res) => {};
+
+const deleteOwner = async(req, res) => {};
+
+export {
+    creatOwner,
+    getAllOwners,
+    getOwnerById,
+    updateOwner,
+    deleteOwner
+}
