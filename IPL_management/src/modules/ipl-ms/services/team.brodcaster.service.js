a47cb166c80f909fb9bc3126b32553e5b2ee2efe
Jai prakash Banjare <jaiprakashbanjare932@gmail.com>
2026-08-01 23:43:27 +0530

    Add ApiResponse import and enhance assignBroadcaster function with error handling

--- IPL_management/src/modules/ipl-ms/services/team.brodcaster.service.js
+++ IPL_management/src/modules/ipl-ms/services/team.brodcaster.service.js
@@ -1,6 +1,7 @@
 import ApiError from "../../../common/utils/api-error";
 import Team from "../models/team.model.js";
 import TeamBroadcaster from "../models/team-broadcaster.model.js"
+import ApiResponse from "../../../common/utils/api-response.js";
 
 
 const assignBroadcaster = async ({teamId, broadcasterId}) => {
@@ -10,4 +11,16 @@ const assignBroadcaster = async ({teamId, broadcasterId}) => {
     }
 
     const broadcaster = await Broadcaster.findById(broadcaster)
+    if(!broadcaster){
+        throw ApiError.notfound("Broadcaster not found");
+    }
+
+    const existing = await TeamBroadcaster.findOne({ teamId, broadcasterId });
+    if(existing){
+        throw ApiError.conflict("Broadcaster already assigned to this team");
+    }
+
+    const teamBroadcaster = await TeamBroadcaster.create({teamId, broadcasterId});
+    return teamBroadcaster;
+
 }
