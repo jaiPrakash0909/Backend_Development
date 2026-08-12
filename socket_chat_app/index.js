24cf9355146d64936c9c3b562ddead95b2b75e77
Jai prakash Banjare <jaiprakashbanjare932@gmail.com>
2026-08-12 23:20:25 +0530

    Add initial HTML structure for chat interface

---
+++ socket_chat_app/index.js
@@ -0,0 +1,21 @@
+import http from 'node:http'
+import { Server } from 'socket.io'
+import path from 'node:path'
+import express from 'express'
+
+async function main() {
+    const app = express();
+    app.use(express.static(path.resolve('./public')))
+
+    const server = http.createServer(app);
+    const io = new Server();
+
+
+    io.attach(server);
+
+    server.listen(9000,() => {
+        console.log(`Http server is running on PORT 9000`)
+    });
+}
+
+main();
