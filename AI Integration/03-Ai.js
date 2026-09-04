15491b901cb035b79698ead264e3d4004ee69c72
Jai prakash Banjare <jaiprakashbanjare932@gmail.com>
2026-09-04 15:59:39 +0530

    Add AI integration script for customer support responses

---
+++ AI Integration/03-Ai.js
@@ -0,0 +1,36 @@
+import { checkOpenAI } from "./01-Ai.js";
+
+const client = await checkOpenAI();
+const model = "gpt-5.4-mini";
+
+console.log(client.baseURL);
+
+async function askQuestion(systemPrompt, userPrompt){
+    const response = await client.chat.completions.create({
+        model,
+        message: [
+            {role: "system", content: systemPrompt},
+            {role: "user", content: userPrompt},
+        ]
+    });
+
+    
+    return response.choices[0].message.content;
+}
+
+const userQuestion = "Where is my food order?";
+
+const fiendly = await askQuestion("You are a friendly customer service agent who loves to help customers with their food orders. You are always polite and eager to assits.", userQuestion);
+
+console.log("++++++ Friendly response ++++++");
+console.log(fiendly);
+
+const formal = await askQuestion("You are a formal customer support agent for a food delivery service. You always respond in a profesional and courteous manner, providing clear and concise information to customers about their orders.", userQuestion);
+
+console.log("++++++ Formal response ++++++");
+console.log(formal);
+
+const rude = await askQuestion("You are a rude customer support agent for a food delivery service. You respond in a curt and unhelpful manner, often providing vague or dismissive answers to customers about their orders", userQuestion);
+
+console.log("++++++ Rude response ++++++");
+console.log(rude);
